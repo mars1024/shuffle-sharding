88d43eef3aa4214c3215b67f855467876ad8a463
Bruce Ma <brucema19901024@gmail.com>
2019-07-08 14:11:07 +0800

    use spatial algorithm to get indices from A array
    Signed-off-by: Bruce Ma <brucema19901024@gmail.com>

--- bruce/main.go
+++ bruce/main.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 )
 
+var handQueue []int
+
 func shuffleSharding(v, queue uint64, handSize int) []int {
 	if handSize < 1 {
 		return nil
@@ -18,12 +20,23 @@ func shuffleSharding(v, queue uint64, handSize int) []int {
 
 	fmt.Printf("A set: %+v \n", ret)
 
+	for i := 0; i < int(queue); i++ {
+		handQueue[i] = 0
+	}
+
 	for i := 0; i < handSize; i++ {
-		for j := 0; j < i; j++ {
-			if ret[j] <= ret[i] {
-				ret[i]++
+		ii, ai := 0, ret[i]+1
+		for {
+			if handQueue[ii] == 0 {
+				ai--
 			}
+			if ai == 0 {
+				handQueue[ii] = 1
+				break
+			}
+			ii++
 		}
+		ret[i] = ii
 	}
 
 	fmt.Printf("I set: %+v \n", ret)
@@ -32,11 +45,12 @@ func shuffleSharding(v, queue uint64, handSize int) []int {
 }
 
 func main() {
-	numQueues := uint64(128)
-	handSize := 6
+	numQueues := uint64(8)
+	handSize := 8
 
 	queue := make([]int, numQueues)
-	retries := 10000
+	handQueue = make([]int, numQueues)
+	retries := 10
 
 	for i := 0; i < retries; i++ {
 		hashValue := rand.Uint64()
